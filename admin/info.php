0b3482594f36486f0f59b3c7b1d9eeb662048a90
Amit Kumar <107535012+useramitkr@users.noreply.github.com>
2024-03-05 10:45:20 +0530

    Create info.php

---
+++ admin/info.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <style>
+
+        th, td {
+            border: 1px solid black;
+            padding: 8px 0;
+            }
+        
+        .titles {
+            background-color: #251A49;
+            color: white;
+        }
+
+        .shortcode-title {
+            font-size: 14px;
+        }
+            
+        .shortcode {
+            font-weight: 550;
+        }
+
+        p {
+            font-size: 14px;
+        }
+        
+    </style>
+</head>
+<body>
+    <div id="pricer-container">
+        <div class="shortcode-info">
+            <h1>Service Pricer</h1>
+            <p>Empower beginners by integrating ServicePricer—a straightforward plugin for website cost calculation and SEO services. Simplify your online presence with transparent and accurate pricing. ServicePricer makes it easy for your audience to understand and engage, enhancing accessibility and user experience effortlessly.</p>
+
+            <h2>Use Shortcodes to insert Cost Calculator.</h2>
+            <table style="width: 50%">
+                <tr class="titles">
+                    <th class="shortcode-title">Calculator Type</th>
+                    <th class="shortcode-title">Short Codes</th>
+                </tr>
+                <tr>
+                    <th>Informatic/Dynamic Website</th>
+                    <th class="shortcode">[infoweb]</th>
+                </tr>
+                <tr>
+                    <th>Ecommerce Website</th>
+                    <th class="shortcode">[ecomm]</th>
+                </tr>
+                <tr>
+                    <th>Website Maintenance</th>
+                    <th class="shortcode">[web-maint]</th>
+                </tr>
+                <tr>
+                    <th>SEO</th>
+                    <th class="shortcode">[seo-calc]</th>
+                </tr>
+            </table>
+
+            <h2>Here are the steps you can follow to use this on your webpage.</h2>
+            <h3>Step: 1</h3>
+            <p>Create a new page or post.</p>
+            <h3>Step: 2</h3>
+            <p>Click on 'Add Block' and insert the 'Shortcodes' block.</p>
+            <h3>Step: 3</h3>
+            <p>Now add the provided shortcode for Service Pricer. Done.</p>
+        </div>
+    </div>
+</body>
+</html>
